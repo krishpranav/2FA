4a459581928eb4b9efe0bf249c6abef32fb595ac
krishpranav <krisna.pranav@gmail.com>
2023-07-09 09:39:58 +0530

    add files via upload

---
+++ 2FA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2FA
+//
+//  Created by krisna pranav on 09/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2FA/_FAApp.swift
@@ -0,0 +1,17 @@
+//
+//  _FAApp.swift
+//  2FA
+//
+//  Created by krisna pranav on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct _FAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
